e68090de18f2ded6eb7e1b7b1a7f4f3bb7f23018
wangjun <wjunzg@gmail.com>
2024-05-16 21:42:32 +0800

    移除不相关的代码

--- main/main.go
+++ main/main.go
@@ -7,25 +7,6 @@ import (
 	"strings"
 )
 
-/*
-func main() {
-	token := flag.String("token", "", "set xddq login token")
-	flag.Parse()
-	if len(strings.TrimSpace(*token)) == 0 {
-		log.Fatalf("token 参数必须")
-	}
-	packet, err := protocol.PacketFromBase64(*token)
-	if err != nil {
-		log.Fatalf("解析token参数失败<%v>", err)
-	}
-	init := protobuf.InitInfo{Token: *token, PlayerID: packet.PlayerID}
-	if err := protobuf.NewSocketClient(init); err != nil {
-		log.Println(err)
-	}
-	fmt.Println()
-}
-*/
-
 func main() {
 	mode := flag.String("mode", "", "set run mode")
 	addr := flag.String("addr", "", "set server addr")
